8c0b4590583bcc18c275870ef944fff1207c11aa
luoxuebinfei <1732806519@qq.com>
2024-01-15 04:07:51 +0800

    建立actix-web模板

---
+++ README.md
@@ -0,0 +1,16 @@
+# 初始化时的选项
+
+```
+✔ Choose which language to use for your frontend · TypeScript / JavaScript - (pnpm, yarn, npm, bun)
+✔ Choose your package manager · pnpm
+✔ Choose your UI template · Vue - (https://vuejs.org)
+✔ Choose your UI flavor · TypeScript
+```
+
+# 更改项目名
+
+```
+./package.json
+./src-tauri/Cargo.toml
+./src-tauri/tauri.conf.json
+```
---
+++ actix-web-template/src-tauri/src/main.rs
@@ -0,0 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod server;
+
+use std::thread;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            let boxed_handle = Box::new(handle);
+            thread::spawn(move || {
+                server::init(*boxed_handle).unwrap();
+            });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
